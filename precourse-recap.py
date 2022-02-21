19ebac561a1a7da63c3bb3a3fed00eab996dd908
CLg003 <clairemjlaing@gmail.com>
2022-02-21 21:26:21 +0000

    first homework commit

---
+++ precourse-recap.py
@@ -0,0 +1,16 @@
+my_name = "Claire"
+print(type(my_name))
+greeting = "Hello "
+welcome = greeting + my_name
+print(welcome.upper())
+print(welcome.lower())
+num_1 = 7
+print(type(num_1))
+num_2 = 5
+total = num_1 * num_2
+print(total)
+price = 2.95
+print(type(price))
+print("The total price is £" + str(price))
+yes = True
+print(type(yes))
